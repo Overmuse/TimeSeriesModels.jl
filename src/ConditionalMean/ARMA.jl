0b3641a8245b21a1215fa1eb61a5304c7a625787
Sebastian Rollen <seb@overmu.se>
2021-02-12 13:39:06 -0500

    WIP

--- src/ConditionalMean/ARMA.jl
+++ src/ConditionalMean/ARMA.jl
@@ -1,45 +1,37 @@
-struct ARIMA{p, d, q, T} <: ConditionalMeanModel{T}
+struct ARMA{p, q, T} <: ConditionalMeanModel{T}
     C::T
     ϕ::Vector{T}
     θ::Vector{T}
 
-    function ARIMA{p, d, q, T}(C, ϕ, θ) where {p, d, q, T}
+    function ARMA{p, q, T}(C, ϕ, θ) where {p, q, T}
         @assert length(ϕ) == p
         @assert length(θ) == q
-        new{p, d, q, T}(C, ϕ, θ)
+        new{p, q, T}(C, ϕ, θ)
     end
 end
 
-const AR{p, T} = ARIMA{p, 0, 0, T}
-const MA{q, T} = ARIMA{0, 0, q, T}
-const ARMA{p, q, T} = ARIMA{p, 0, q, T}
+const AR{p, T} = ARMA{p, 0, T}
+const MA{q, T} = ARMA{0, q, T}
+const ARMA{p, q, T} = ARMA{p, q, T}
 
-function ARIMA{p, d, q}(coefficients::Vector{T}) where {p, d, q, T}
-    @assert length(coefficients) == (1 + p + q)
-    ARIMA{p, d, q, T}(coefficients[1], coefficients[2:p+1], coefficients[p+2:end])
+function ARMA{p, q}(coefficients::Vector{T}) where {p, q, T}
+    @assert length(coefficients) == (p + q + 1)
+    ARMA{p, q, T}(coefficients[1], coefficients[2:p+1], coefficients[p+2:end])
 end
 
-function is_valid(model::ARIMA{p, d, q}) where {p, d, q}
+function is_valid(model::ARMA{p, q}) where {p, q}
     all(<=(1), abs.(model.ϕ)) && all(<=(1), abs.(model.θ))
 end
 
-function nparams(::Type{ARIMA{p, d, q}}) where {p, d, q}
-    p + q + 1
+function nparams(::Type{ARMA{p, q}}) where {p, q}
+    1 + p + q
 end
 
-function difference(D, y)
-    if D == 0
-        y
-    else
-        difference(D-1, y[2:end] .- y[1:end-1])
-    end
+function presamples(model::ARMA{p, q}) where {p, q}
+    max(p, q)
 end
 
-function presamples(model::ARIMA{p, d, q}) where {p, d, q}
-    d + max(p, q)
-end
-
-function initial_coefficients(::Type{ARIMA{p, d, q}}, y::Vector{T}) where {p, d, q, T}
+function initial_coefficients(::Type{ARMA{p, q}}, y::Vector{T}) where {p, q, T}
     N = length(y)
     X = Matrix{T}(undef, N - p, p + 1)
     X[:, 1] .= ones(T)
@@ -50,8 +42,8 @@ function initial_coefficients(::Type{ARIMA{p, d, q}}, y::Vector{T}) where {p, d,
     vcat(ϕ, zeros(T, q))
 end
 
-function conditional_mean(model::ARIMA{p, d, q, T}, y, ϵ) where {p, d, q, T}
-    ŷ = zeros(T, length(y) - d)
+function conditional_mean(model::ARMA{p, q, T}, y, ϵ) where {p, q, T}
+    ŷ = zeros(T, length(y))
     ŷ .= model.C
     for t in 2:length(y)
         if t > p
@@ -68,11 +60,11 @@ function conditional_mean(model::ARIMA{p, d, q, T}, y, ϵ) where {p, d, q, T}
     ŷ
 end
 
-function residuals(model::ARIMA, y)
+function residuals(model::ARMA, y)
     y .- filter(model, y)
 end
 
-function simulate(model::ARIMA{p, d, q}, t) where {p, d, q}
+function simulate(model::ARMA{p, q}, t) where {p, q}
     y = zeros(t)
     ϵ = zeros(t)
     y[1] = model.C
