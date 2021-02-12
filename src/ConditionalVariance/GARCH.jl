5aa0ff768e7a41f2fea0c56b127a80152ed07fbe
Sebastian Rollen <seb@overmu.se>
2021-02-12 12:13:11 -0500

    functioning GARCH models

--- src/ConditionalVariance/GARCH.jl
+++ src/ConditionalVariance/GARCH.jl
@@ -1,12 +1,12 @@
 struct GARCH{p, q, T} <: ConditionalVarianceModel{T}
     ω::T
-    alphas::Vector{T}
     betas::Vector{T}
+    alphas::Vector{T}
 
-    function GARCH{p, q, T}(ω, alphas, betas) where {p, q, T}
-        @assert(p == length(alphas))
-        @assert(q == length(betas))
-        new{p, q, T}(ω, alphas, betas)
+    function GARCH{p, q, T}(ω, betas, alphas) where {p, q, T}
+        @assert(q == length(alphas))
+        @assert(p == length(betas))
+        new{p, q, T}(ω, betas, alphas)
     end
 end
 
@@ -14,7 +14,7 @@ function GARCH{p, q}(coef::Vector{T}) where {p, q, T}
     GARCH{p, q, T}(coef[1], coef[2:1+p], coef[2+p:end])
 end
 
-const ARCH{p, T} = GARCH{p, 0, T}
+const ARCH{q, T} = GARCH{0, q, T}
 
 function is_valid(model::GARCH)
     model.ω > 0 && all(x -> 0 <= x <= 1, model.alphas) && all(x -> 0 <= x <= 1, model.betas)
@@ -29,25 +29,25 @@ function presamples(::GARCH{p, q}) where {p, q}
 end
 
 function initial_coefficients(::Type{GARCH{p, q}}, y) where {p, q}
-    alphas = fill(0.9 / p, p)
-    betas = fill(0.05 / q, q)
+    betas = fill(0.9 / p, p)
+    alphas = fill(0.05 / q, q)
     ω = var(y) * (1.0 - sum(alphas) - sum(betas))
-    vcat(ω, alphas, betas)
+    vcat(ω, betas, alphas)
 end
 
-function conditional_variance(model::GARCH{p, q, T}, y::Vector) where {p, q, T}
-    t = length(y)
+function conditional_variance(model::GARCH{p, q, T}, ϵ::Vector, σ̂) where {p, q, T}
+    # TODO: Remove σ₀ parameter somehow
+    t = length(ϵ)
     σ² = zeros(T, t)
     n = presamples(model)
 
-    σ²[1] = unconditional_variance(model)
+    σ² .= model.ω
     for i in (n+1):t
-        σ²[i] = model.ω
-        for (j, α) in enumerate(model.alphas)
-            σ²[i] += α * y[i - j] ^ 2
+        for j in 1:p
+            σ²[i] += model.betas[j] * σ̂[i - j] ^ 2
         end
-        for (j, β) in enumerate(model.betas)
-            σ²[i] += β * σ²[i - j]
+        for j in 1:q
+            σ²[i] += model.alphas[j] * ϵ[i - j] ^ 2
         end
     end
     σ²
